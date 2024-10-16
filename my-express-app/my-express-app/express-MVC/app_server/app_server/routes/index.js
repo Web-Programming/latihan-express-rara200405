2b3d100caaaae2628b151873ae4bae0bfdb78a0c
kiarraaaaaa <beneciakiarra_2226240052@mhs.mdp.ac.id>
2024-10-16 09:42:03 +0700

    add : db.js
    menambhakan file db.js

---
+++ my-express-app/my-express-app/express-MVC/app_server/app_server/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const mainControllers=require("../controllers/main");
+
+
+/* GET home page. */
+router.get('/',mainControllers.index);
+// buat route ke halaman kontak(dengan method get)
+router.get('/kontak', mainControllers.kontak);
+//router.get('/kontak3',mainControllers.kontak3);
+module.exports = router;
